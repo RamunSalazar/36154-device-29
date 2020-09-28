1108e97de4ee7a79b40152e2ac10926a225a8ea8
RamunSalazar <52279521+RamunSalazar@users.noreply.github.com>
2020-09-28 15:34:30 +0300

    Добавил флекс сетку на внутреннюю страницу

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',timer);
+function display__over() {
+  document.querySelector('.header__catalog-wrap').style.display='grid';
+}
+function display__out() {
+  document.querySelector('.header__catalog-wrap').style.display='none';
+}
