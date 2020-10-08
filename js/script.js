cc97af6358da5374552f73cc9ff96c6859d3f672
RamunSalazar <52279521+RamunSalazar@users.noreply.github.com>
2020-10-08 18:51:03 +0300

    внес правки в финальную верстку главной страницы

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 function promo_click_button(elem) {
   var arr_btn = document.querySelectorAll('.promo__button');
   var arr_slide = document.querySelectorAll('.promo__slide');
+  var arr_btn_wrap = document.querySelectorAll('.promo__button-wrap');
   var index = 0;
   var slide_index = 0;
   for (var i=0; i<arr_btn.length; i++) {
     if (arr_btn[i].classList.contains('promo-visibility')) {
-      index = i;
       arr_btn[i].classList.remove('promo-visibility');
     }
   }
-  elem.classList.add('promo-visibility');
-  arr_slide[index].style.visibility = 'hidden';
-  for (var i=0; i<arr_btn.length; i++) {
-    if (arr_btn[i] == elem) {
-      slide_index = i;
+  for (var i=0; i<arr_btn_wrap.length; i++) {
+    for (var j=0; j<arr_btn_wrap[i].children.length; j++) {
+      if (arr_btn_wrap[i].children[j] == elem) {
+        arr_btn_wrap[j].children[j].classList.add('promo-visibility');
+        index = i;
+        break;
+      }
     }
   }
-  arr_slide[slide_index].style.visibility = 'visible';
+  arr_slide[index].style.display = 'none';
+  for (var i=0; i<arr_btn_wrap.length; i++) {
+    for (var j=0; j<arr_btn_wrap[i].children.length; j++) {
+      if (arr_btn_wrap[i].children[j] == elem) {
+        slide_index = j;
+        break;
+      }
+    }
+  }
+  arr_slide[slide_index].style.display = 'grid';
 }
 
 function service_click_button(elem) {
@@ -40,17 +51,21 @@ function service_click_button(elem) {
 }
 
 function modal_feedback_display_on() {
-  document.querySelector('.modal-feedback').style.display="block";
+  document.querySelector('.modal__feedback-wrap').style.display="flex";
+  document.querySelector('.page__body').style.overflow="hidden";
 }
 
 function modal_feedback_close() {
-  document.querySelector('.modal-feedback').style.display="none";
+  document.querySelector('.modal__feedback-wrap').style.display="none";
+  document.querySelector('.page__body').style.overflow="visible";
 }
 
 function modal_map_display_on() {
-  document.querySelector('.modal-map').style.display="block";
+  document.querySelector('.modal__map-wrap').style.display="flex";
+  document.querySelector('.page__body').style.overflow="hidden";
 }
 
 function modal_map_close() {
-  document.querySelector('.modal-map').style.display="none";
+  document.querySelector('.modal__map-wrap').style.display="none";
+  document.querySelector('.page__body').style.overflow="visible";
 }
